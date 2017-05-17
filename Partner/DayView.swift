e8fc722d57392e5a33ddd05abfd8866423ec2cbc
Joseph Speakman <josephspeakman@josephs-macbook-pro.local>
2017-05-17 00:30:02 -0400

    Added code

---
+++ Partner/DayView.swift
@@ -0,0 +1,21 @@
+//
+//  DayView.swift
+//  Partner
+//
+//  Created by Joseph Speakman on 5/16/17.
+//  Copyright © 2017 Joe Speakman. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class DayView: UIView {
+
+
+override func draw(_ rect: CGRect) {
+
+
+    }
+
+
+}
---
+++ Partner/ScheduleTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleTableViewCell.swift
+//  
+//
+//  Created by Joseph Speakman on 5/16/17.
+//
+//
+
+import UIKit
+
+class ScheduleTableViewCell: UITableViewCell {
+    @IBOutlet weak var shiftStore: UILabel!
+    @IBOutlet weak var shiftTime: UILabel!
+    @IBOutlet weak var DayNumber: UIImageView!
+    
+    
+}
